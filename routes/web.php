3b84f8a0d851f8460571f20526634ac29fe05285
Candra <candraherdiansyah14@gmail.com>
2019-07-02 15:15:48 +0700

    tambah table kelas

--- routes/web.php
+++ routes/web.php
@@ -28,3 +28,7 @@ Route::get('api', function () {
 Auth::routes();
 
 Route::get('/home', 'HomeController@index')->name('home');
+
+Auth::routes();
+
+Route::get('/home', 'HomeController@index')->name('home');
