9f9757b273faab9996520c08b1ad2a88d93e61ba
Candra <candraherdiansyah14@gmail.com>
2019-07-02 11:31:08 +0700

    Projek Heroku pertama

---
+++ routes/web.php
@@ -0,0 +1,26 @@
+<?php
+
+/*
+|--------------------------------------------------------------------------
+| Web Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register web routes for your application. These
+| routes are loaded by the RouteServiceProvider within a group which
+| contains the "web" middleware group. Now create something great!
+|
+*/
+
+Route::get('/', function () {
+    return view('welcome');
+});
+
+Route::get('api', function () {
+    $a = ['dadang', 'abdul', 'usep'];
+    $response = [
+        'success' => true,
+        'data'  => $a,
+        'message' => 'berhasil'
+    ];
+    return response()->json($response, 200);
+});
