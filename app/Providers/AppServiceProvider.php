b2374953430ab53961b5952b8b52be17d05a5097
Candra <candraherdiansyah14@gmail.com>
2019-07-02 13:28:05 +0700

    latihan error

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -14,7 +14,7 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register()
     {
-        schema::defaultStringLength(191)
+        schema::defaultStringLength(191);
     }
 
     /**
