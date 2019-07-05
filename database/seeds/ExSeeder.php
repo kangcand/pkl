d199a4831b7e3d942001a27c5fd9b63e0bd7a9bb
Candra <candraherdiansyah14@gmail.com>
2019-07-05 13:31:57 +0700

    konek github

--- database/seeds/ExSeeder.php
+++ database/seeds/ExSeeder.php
@@ -20,21 +20,21 @@ class ExSeeder extends Seeder
             'password' => bcrypt('rahasia')
         ]);
 
-        $faker = Faker::create('id_ID');
-        for ($i = 0; $i < 5; $i++) {
-            $gender = $faker->randomElement(['male', 'female']);
-            $siswa = new Siswa;
-            $siswa->nama = $faker->name($gender);
-            $siswa->nis = $faker->nik; //$faker->numberBetween($min = 10000, $max = 90000);
-            if ($gender === "male") {
-                $gender = "Laki-laki";
-            } elseif ($gender == "female") {
-                $gender = "Perempuan";
-            }
-            $siswa->jenis_kelamin = $gender;
-            $siswa->alamat = $faker->address;
-            $siswa->tgl_lahir = $faker->date;
-            $siswa->save();
-        }
+        // $faker = Faker::create('id_ID');
+        // for ($i = 0; $i < 5; $i++) {
+        //     $gender = $faker->randomElement(['male', 'female']);
+        //     $siswa = new Siswa;
+        //     $siswa->nama = $faker->name($gender);
+        //     $siswa->nis = $faker->nik; //$faker->numberBetween($min = 10000, $max = 90000);
+        //     if ($gender === "male") {
+        //         $gender = "Laki-laki";
+        //     } elseif ($gender == "female") {
+        //         $gender = "Perempuan";
+        //     }
+        //     $siswa->jenis_kelamin = $gender;
+        //     $siswa->alamat = $faker->address;
+        //     $siswa->tgl_lahir = $faker->date;
+        //     $siswa->save();
     }
+}
 }
